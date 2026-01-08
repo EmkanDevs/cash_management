3b3380338a75513419f5ccf97a320b335ecad899
EmkanDevs <emkan.erpnext@gmail.com>
2026-01-08 09:16:25 +0300

    Payment Requester

--- cash_management/cash_management/doctype/payment_requester/payment_requester.js
+++ cash_management/cash_management/doctype/payment_requester/payment_requester.js
@@ -1,8 +1,27 @@
-// Copyright (c) 2025, chris.panikulangara@finbyz.tech and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Payment Requester", {
+    party: function(frm) {
 
-// frappe.ui.form.on("Payment Requester", {
-// 	refresh(frm) {
+        if (!frm.doc.party || !frm.doc.party_type) {
+            frm.set_value("party_name", "");
+            return;
+        }
 
-// 	},
-// });
+        let name_field_map = {
+            "Customer": "customer_name",
+            "Supplier": "supplier_name",
+            "Employee": "employee_name",
+            "Lead": "lead_name"
+        };
+
+        let name_field = name_field_map[frm.doc.party_type] || "name";
+
+        frappe.db.get_value(
+            frm.doc.party_type,
+            frm.doc.party,
+            name_field,
+            (r) => {
+                frm.set_value("party_name", r[name_field] || "");
+            }
+        );
+    }
+});
